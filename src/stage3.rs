0a1d1fed0ea4e86bb39ff40599c10da8b4e8dcff
Philpax <me@philpax.me>
2024-04-28 02:34:24 +0200

    feat: instruction format xml output

--- src/stage3.rs
+++ src/stage3.rs
@@ -1,16 +1,18 @@
 //! Produces the final training dataset.
 
-use std::{fmt::Write, io::BufRead, io::Write as IoWrite};
+use std::{io::BufRead, io::Write as IoWrite};
 
 use rand::prelude::SliceRandom;
 use serde::Serialize;
 use unicode_normalization::UnicodeNormalization;
+use xml::writer::XmlEvent;
 
 use super::{Submission, Verdict, STAGE2_OUTPUT_PATH, STAGE3_OUTPUT_PATH};
 
 #[derive(Serialize)]
 struct DataOut {
-    text: String,
+    instruction: String,
+    output: String,
 }
 
 pub fn run() -> anyhow::Result<()> {
@@ -31,43 +33,42 @@ pub fn run() -> anyhow::Result<()> {
 
         let data: Submission = serde_json::from_str(&line?)?;
 
-        let mut text = String::new();
-        writeln!(text, "### Title:")?;
-        writeln!(text)?;
-        writeln!(text, "{}\n", normalize(&data.title))?;
+        let mut text = vec![];
+        let mut writer = xml::writer::EmitterConfig::new()
+            .perform_indent(true)
+            .write_document_declaration(false)
+            .create_writer(&mut text);
 
-        writeln!(text, "### Text:")?;
-        writeln!(text)?;
-        writeln!(text, "{}\n", normalize(&data.text))?;
-
-        writeln!(text, "### Comments:")?;
-        writeln!(text)?;
+        writer.write(XmlEvent::start_element("text"))?;
+        writer.write(XmlEvent::characters(&normalize(&data.text)))?;
+        writer.write(XmlEvent::end_element())?;
 
         let mut comments = data.comments.values().collect::<Vec<_>>();
         comments.sort_by_key(|c| -c.score);
         comments.truncate(5);
         comments.shuffle(&mut rand::thread_rng());
 
-        for (idx, comment) in comments.iter().enumerate() {
-            writeln!(text, "#### Person {}:", idx + 1)?;
-            writeln!(text, "{}", normalize(&comment.body))?;
-            writeln!(text)?;
+        writer.write(XmlEvent::start_element("comments"))?;
+        for comment in comments {
+            writer.write(XmlEvent::start_element("comment"))?;
+            writer.write(XmlEvent::characters(&normalize(&comment.body)))?;
+            writer.write(XmlEvent::end_element())?;
         }
+        writer.write(XmlEvent::end_element())?;
 
-        writeln!(text, "### Verdict:")?;
-        writeln!(text)?;
-        write!(
-            text,
-            "{}",
-            match data.verdict {
-                Verdict::NotTheAsshole => "NTA",
-                Verdict::NoAssholesHere => "NAH",
-                Verdict::EveryoneSucks => "ESH",
-                Verdict::Asshole => "YTA",
-            }
-        )?;
+        writer.write(XmlEvent::start_element("verdict"))?;
+        writer.write(XmlEvent::characters(&match data.verdict {
+            Verdict::NotTheAsshole => "NTA",
+            Verdict::NoAssholesHere => "NAH",
+            Verdict::EveryoneSucks => "ESH",
+            Verdict::Asshole => "YTA",
+        }))?;
+        writer.write(XmlEvent::end_element())?;
 
-        let output = serde_json::to_string(&DataOut { text })?;
+        let output = serde_json::to_string(&DataOut {
+            instruction: normalize(&data.title),
+            output: String::from_utf8_lossy(&text).into_owned(),
+        })?;
         writeln!(output_file, "{}", output)?;
     }
     println!(
